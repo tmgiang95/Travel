56c06481f22307722f095e915a264787659839b1
tmgiang95 <32532855+tmgiang95@users.noreply.github.com>
2018-12-26 18:10:23 +0700

    Add files via upload

--- app/src/main/java/com/example/admin/travel/base/BaseActivity.java
+++ app/src/main/java/com/example/admin/travel/base/BaseActivity.java
@@ -8,6 +8,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
     private static final String TAG =  BaseActivity.class.getSimpleName();
+    public static final int REQCD_PLACE_AUTOCOMPLETE = 1000;
+    public static final int REQCD_TRAVEL_ADD = 2000;
+    public static final int REQCD_TRAVEL_EDIT = 2001;
+    public static final String REQKEY_TRAVEL = "REQKEY_TRAVEL";
+    public static final String REQACTION_EDIT_TRAVEL = "REQACTION_EDIT_TRAVEL";
+    public static final String REQACTION_DEL_TRAVEL = "REQACTION_DEL_TRAVEL";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
